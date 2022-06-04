302801d78babdd814a257a37d229117376639774
Egor Malov <egor.malov2014@gmail.com>
2022-06-04 18:52:12 +0800

    add urls main page, contacts, about

--- app/core/views.py
+++ app/core/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class MainPage(View):
+
+    def get(self, request):
+        return render(request, 'main_page/main_page.html', {})
+
+
+class ContactsView(View):
+
+    def get(self, request):
+        return render(request, 'main_page/contacts.html', {})
+
+
+class AboutView(View):
+
+    def get(self, request):
+        return render(request, 'main_page/about.html', {})
+
