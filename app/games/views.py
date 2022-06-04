66ecc80cc27a3dd9d4489445cbc8b3cf6c6074d4
Egor Malov <egor.malov2014@gmail.com>
2022-06-04 18:53:49 +0800

    add urls stroop, schulte, allgames, leaderboard

--- app/games/views.py
+++ app/games/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class SchulteGame(View):
+
+    def get(self, request):
+        return render(request, 'games/schulte.html', {})
+
+
+class StroopGame(View):
+
+    def get(self, request):
+        return render(request, 'games/stroop.html', {})
+
+
+class AllGames(View):
+
+    def get(self, request):
+        return render(request, 'games/allgames.html', {})
+
+
+class LeaderboardsView(View):
+
+    def get(self, request):
+        return render(request, 'leaderboards/leaderboard.html', {})
