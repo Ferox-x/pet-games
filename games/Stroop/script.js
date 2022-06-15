f621d9b7cd2cd76e61878552b4f44b6464ce8a57
Alexander Kondratev <kondratev.av@inbox.ru>
2022-06-15 23:50:30 +0500

    Creating main logic, now its work!?

--- games/Stroop/script.js
+++ games/Stroop/script.js
@@ -1,26 +1,67 @@
 const colorName = document.getElementById('wordBlock');
 
-
+let prewColorName = 'nameColor'
 let arrayColorsName = ['Blue', 'Black', 'Green', 'Red', 'Yellow'];
 let arrayColors = ['blue', 'black', 'green', 'red', 'yellow'];
+let correctCounter = 0
+let totalCounter = 0
 
-function randomColorName() {
-    let colorName =  Math.floor(Math.random() * arrayColors.length);
+function randomColor(array) {
+    let colorName =  Math.floor(Math.random() * array.length);
     return arrayColorsName[colorName]
 }
 
-function randomColor() {
-    let colorType = Math.floor(Math.random() * arrayColors.length);
-    return arrayColorsName[colorType]
+function correctClick(event) {
+    const colorDict = {
+        'blue': 'Blue',
+        'black': 'Black',
+        'green': 'Green',
+        'red': 'Red',
+        'yellow': 'Yellow'
+    }
+    let colorNameP = document.getElementById('block__p')
+    let colorNameText = colorNameP.textContent
+    let divId = event.target.id
+    let selectedDiv = document.getElementById(divId);
+    let color = selectedDiv.style.backgroundColor
+    let result = colorDict[color]
+    if (result === colorNameText) {
+        correctCounter++
+    }
+    colorNameP.remove()
+    let listColor = generateColors()
+    createElementP(listColor[0], listColor[1])
+    totalCounter++
+    console.log(totalCounter, correctCounter)
+}
 
+function generateColors() {
+    let currentColorName = randomColor(arrayColorsName);
+    let currentColor = randomColor(arrayColors);
+
+    do {
+        currentColorName = randomColor(arrayColorsName);
+    } while (currentColorName === prewColorName)
+    return [currentColor, currentColorName]
+}
+
+function createElementP(currentColor, currentColorName) {
+
+    let colorNameP = document.createElement('p')
+    colorNameP.innerHTML = currentColorName
+    colorNameP.style.color = currentColor
+    colorNameP.className = 'world-block__p'
+    colorNameP.id = 'block__p'
+    colorName.appendChild(colorNameP)
+    prewColorName = currentColorName
 }
 
 function draw() {
-    let colorNameDiv = document.createElement('p')
-    colorNameDiv.innerHTML = randomColorName();
-    colorNameDiv.style.color = randomColor();
-    colorNameDiv.className = 'world-block__p'
-    colorName.appendChild(colorNameDiv)
+    let listColor = generateColors()
+    createElementP(listColor[0], listColor[1])
+
+    for (let j=1; j<=5; j++) {
+        document.getElementById(String(j)).onclick = correctClick;
+    }
 }
 draw()
-
