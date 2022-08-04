87e003003225a1771d2cc8fa09b4f8ddee136db7
Alexander Kondratev <kondratev.av@inbox.ru>
2022-08-04 18:33:52 +0500

    changing result display system

--- app/games/models.py
+++ app/games/models.py
@@ -5,31 +5,50 @@ from core.models import CustomerUser
 
 class SchulteModel(models.Model):
     user = models.ForeignKey(
-        CustomerUser, verbose_name='Пользователь', on_delete=models.CASCADE,
+        CustomerUser,
+        verbose_name='Пользователь',
+        on_delete=models.CASCADE,
     )
 
     record = models.CharField(
-        max_length=10, verbose_name='Время',
+        max_length=10,
+        verbose_name='Время',
     )
 
     date = models.DateTimeField(
-        verbose_name='Время', auto_now=True, editable=False
+        verbose_name='Время',
+        auto_now=True,
+        editable=False
     )
+
+    class Meta:
+        db_table = 'games_schulte'
+        verbose_name = 'Schulte'
 
 
 class StroopModel(models.Model):
     user = models.ForeignKey(
-        CustomerUser, verbose_name='Пользователь', on_delete=models.CASCADE,
+        CustomerUser,
+        verbose_name='Пользователь',
+        on_delete=models.CASCADE,
     )
 
     result = models.CharField(
-        max_length=32, verbose_name='Результат'
+        max_length=32,
+        verbose_name='Результат'
     )
 
     score = models.CharField(
-        max_length=32, verbose_name='Время',
+        max_length=32,
+        verbose_name='Счет',
     )
 
     date = models.DateTimeField(
-        verbose_name='Время', auto_now=True, editable=False
+        verbose_name='Время',
+        auto_now=True,
+        editable=False
     )
+
+    class Meta:
+        db_table = 'games_stroop'
+        verbose_name = 'Stroop'
