c02c272935e5e7206c98ddffa0f95c7a8a64bbe4
Egor Malov <99792949+Ferox-x@users.noreply.github.com>
2022-08-18 17:24:33 +0800

    Delete games/Train memory directory

--- games/Train memory/clear_words.py
+++
@@ -1,6 +0,0 @@
-
-with open('Слова.txt', 'r', encoding='utf-8') as file:
-    with open('clean.txt', 'w', encoding='utf-8') as clean_file:
-        for line in file:
-            string = f"'{line.split()[1]}'," + " "
-            clean_file.write(string)
